68a16d32529177ae4728f9c7d567dc679aff6b4f
Sergey <lazybark.dev@gmail.com>
2022-08-25 16:42:11 +0300

    Main.go with all testing stuff

--- main.go
+++ main.go
@@ -11,7 +11,12 @@ import (
 )
 
 func main() {
-	cli := logger.NewCLI(events.ErrorFlow)
+	//Create  loggers (non-default loggers would just need to implement logger.ILogger interface)
+	//CLI logger for all events
+	cli := logger.NewCLI(events.Any)
+	//CLI for errors only
+	cli2 := logger.NewCLI(events.ErrorFlow)
+	//Text loggers for all event types
 	ptl, err := logger.NewPlaintext("some.log", false, events.Any)
 	if err != nil {
 		log.Fatal(err)
@@ -24,26 +29,51 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	p := lproc.New("", make(chan error), false, cli, ptl, js, csv)
+	//New LogProcessor to rule them all
+	p := lproc.New("", make(chan error), false, cli, cli2, ptl, js, csv)
 
-	p.Log(events.Info("Some info"))
-
+	//Log simple messages with default parameters
+	//.Verbose() here to avoid doubles in CLI output as we have two CLI loggers above
+	p.Log(events.Info("Some info").Verbose())
+	p.Log(events.Warning("Some warning").Verbose())
+	p.Log(events.Error("Some error").Verbose())
 	time.Sleep(2 * time.Second)
-	p.Log(events.Info("Some info2"))
+	p.Log(events.Info("Some info2").Verbose())
+
+	//Now add some default event sources
+	p.Log(events.Error("Some error").Src(events.EvsDebug).Verbose())
+	p.Log(events.Error("Some error").Src(events.EvsMain).Verbose())
+	//And create non-default source
+	weird := events.Source{
+		Text:  "WEIRD SOURCE",
+		Open:  "<",
+		Close: ">",
+	}
+	p.Log(events.Error("That one has come from weird place").Src(weird).Verbose())
 
 	//Using of TimeFixed
-	e := events.Error("Some error occured").FixTime()
+	e := events.Error("Some error occured").FixTime().Verbose()
 	p.Log(e)
 	time.Sleep(2 * time.Second)
 	p.Log(e.SetText("And the same moment something else happened!"))
 
+	//Set one of default formats (non-default can be processes only by custom loggers)
+	p.Log(events.Info("Red info message").Red())
+
 	//Using LogErrOnly
-	//Should be logged, but only to CLI
+	//Should be logged, but will have doubles in CLI
 	p.LogErrOnly(GetErr(), events.ErrorFlow)
 	p.LogErrOnly(GetErrEvent(), events.ErrorFlow)
 	//Should NOT be logged
 	p.LogErrOnly(GetString(), events.ErrorFlow)
 	p.LogErrOnly(GetNonErrEvent(), events.ErrorFlow)
+
+	ed := events.EvDefault{
+		Source: events.EvsMain,
+		Type:   events.Debug,
+		Format: events.None,
+	}
+	p.Log(ed.Note("Event from default template"))
 
 }
 
